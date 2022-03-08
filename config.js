ef1e158798d55f0ebb05cafeb964d3973d37cf34
BuiPhucNguyen <99703790+BuiPhucNguyen@users.noreply.github.com>
2022-03-08 10:27:35 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ngọc Minh';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100013121402427';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = '🖤IU BAKE🖤';
+const contentCard = ' Chúc Bake 1 ngày 8-3 vui vẻ nhoaaa. Yêuuuuuuuuuu.🖤';
+
+const giftImage = 'bake.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
